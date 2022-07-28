ba30d8180f6b99bf60259d41d70e582c0985493a
elegator114 <60179996+elegator114@users.noreply.github.com>
2022-07-29 00:03:01 +0900

    ㅕ

---
+++ javascript/js/common.js
@@ -0,0 +1,20 @@
+// js 파일에서 "/**" 이라고 치면 아래 5줄 내용이 정리됨
+/**
+ *
+ * @param {*} productAmount
+ * @returns
+ */
+
+function taxAmount(productAmount) {
+  // 부가세 10% 가정
+  const tax = 0.000001;
+  return productAmount * tax;
+}
+
+function getTop5() {
+  return;
+}
+
+function aaa() {
+  return "aaaaa";
+}
